7abf727c00469de72b8c61a36da720dbec2d0a63
dazui-py <joelmercruz769+@gmail.com>
2026-03-28 20:34:58 +0000

    removed fixed twrp_a14.mk

--- twrp_a14m.mk
+++ twrp_a14m.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/a14m/device.mk)
 
 PRODUCT_DEVICE := a14m
-PRODUCT_NAME := orangefox_a14m
+PRODUCT_NAME := twrp_a14m
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A145R
 PRODUCT_MANUFACTURER := samsung
